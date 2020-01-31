dd3290f1054acdfec1069432587271fc1314d125
SharanyaAntony <60436431+SharanyaAntony@users.noreply.github.com>
2020-02-01 01:20:59 +0530

    Add files via upload

---
+++ removing char.py
@@ -0,0 +1,11 @@
+# removing chars
+
+
+st=input("razak mohammad");
+for ch in range(0,len(st)):
+    num=int(input("Enter the number: "))
+    if num<len(st)
+    del st[num]
+print(st)
+
+#incorrect
